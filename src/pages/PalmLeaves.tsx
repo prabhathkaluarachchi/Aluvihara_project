b1a521b3dc938302bf447314f899fe755f98f023
pasindu526 <pasindumadusanka526@gmail.com>
2025-05-28 15:43:16 +0530

    Update v2

--- src/pages/PalmLeaves.tsx
+++ src/pages/PalmLeaves.tsx
@@ -1,10 +1,10 @@
-import Signatures from "../component/palm/Signatures"
-import Tech from "../component/palm/Tech"
-import Palm_Hero from "../component/palm/Palm_Hero"
-import Process from "../component/palm/Process"
-import NavBar1 from "../component/common/NavBar1"
-import Footer from "../component/common/Footer"
-import { useEffect } from "react"
+import Signatures from "../component/palm/Signatures";
+import Tech from "../component/palm/Tech";
+import Palm_Hero from "../component/palm/Palm_Hero";
+import Process from "../component/palm/Process";
+import NavBar1 from "../component/common/NavBar1";
+import Footer from "../component/common/Footer";
+import { useEffect } from "react";
 
 const PalmLeaves: React.FC = () => {
   //top mount
@@ -16,28 +16,25 @@ const PalmLeaves: React.FC = () => {
     <>
       <header>
         {/* <NavBar /> */}
-        <NavBar1 page="palm"/>
+        <NavBar1 page="palm" />
 
         {/* hero section */}
         <section className="h-auto w-full">
-         
-          <Palm_Hero/>
+          <Palm_Hero />
         </section>
       </header>
-        
-        <Tech/>
-        <Process/>
-        <Signatures/>
 
+      <Tech />
+      <Process />
+      <Signatures />
 
-            {/* footer section */}
+      {/* footer section */}
       <div className="mt-30">
         <footer className="bg-[#222222] py-10">
           <Footer />
         </footer>
         {/* copyrights */}
-        <div
-          className="bg-[#1E1E1E] text-center py-3 text-[8px] md:text-xs text-ternary">
+        <div className="bg-[#1E1E1E] text-center py-3 text-[8px] md:text-xs text-ternary">
           <p>All rights reserved | Powered by All In One Holdings</p>
         </div>
       </div>
